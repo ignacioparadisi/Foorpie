e44e5e5b7b0a3f42fbc7e0b44b4686f42ccb4741
Ignacio Paradisi <ignacioparadisi@gmail.com>
2020-09-19 20:09:17 -0400

    Rename app

---
+++ Foorpie/Extensions/UIColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extension.swift
+//  Cottura
+//
+//  Created by Ignacio Paradisi on 8/27/20.
+//  Copyright © 2020 Ignacio Paradisi. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    /// Dark gray system color for alert's text color
+    static var systemDarkGray = UIColor(named: "systemDarkGray")!
+    static var accent: UIColor = .systemGreen
+}
